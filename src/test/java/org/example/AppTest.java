5f41060901456e1a82d5f5fd008a161668f91830
gkwgu <polina.petr0v4@yandex.ru>
2025-12-11 22:11:57 +0300

    Add Tests, fix pom.xml

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -7,7 +7,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    void myFirstTest() {
+    void reverseLetters_shouldReturnReverseString_ifContainsString() {
         assertEquals("t@eb eht av$J!123", new StringUtil().reverseLetters("J@va the be$t!123"));
+    }
+
+    @Test
+    void reverseLetters_shouldReturnEmptyString_ifContainsEmptyString(){
+        assertEquals("",new StringUtil().reverseLetters(""));
+    }
+    @Test
+    void reverseLetters_shouldReturnEmptyString_ifContainsNullInput() {
+        assertEquals("", new StringUtil().reverseLetters(null));
+    }
+
+    @Test
+    void reverseLetters_shouldReturnReverseString_ifContainsOneLetter() {
+        assertEquals("a", new StringUtil().reverseLetters("a"));
+    }
+
+    @Test
+    void reverseLetters_shouldReturnString_ifContainsOnlyNoLetters() {
+        assertEquals("123!@#", new StringUtil().reverseLetters("123!@#"));
+    }
+
+    @Test
+    void reverseLetters_shouldReturnString_ifContainsWithSpacesAndPunctuation() {
+        assertEquals("egAelp! miS", new StringUtil().reverseLetters("Simple! Age"));
     }
 }
